a7794fa6b220eebfae7269a3e5682fff012ddeba
ak47 <kokitkarak@gmail.com>
2022-01-01 22:06:00 +0530

    added hash table problems

---
+++ HashTableProblems/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HashTableProblems;
+
+namespace HashTableProblems
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(18);
+            hash.Add("0", "Paranoids");
+            hash.Add("1", "are");
+            hash.Add("2", "not");
+            hash.Add("3", "paranoid");
+            hash.Add("4", "because");
+            hash.Add("5", "they");
+            hash.Add("6", "are");
+            hash.Add("7", "paranoid");
+            hash.Add("8", "but");
+            hash.Add("9", "because");
+            hash.Add("10", "they");
+            hash.Add("11", "keep");
+            hash.Add("12", "putting");
+            hash.Add("13", "themselves");
+            hash.Add("14", "delibrately");
+            hash.Add("15", "into");
+            hash.Add("16", "paranoid");
+            hash.Add("17", "avoidable");
+            hash.Add("18", "situation");
+            string hash18 = hash.Get("18");
+            Console.WriteLine("18th index value: " + hash18);
+
+            hash.Remove("17");
+            string hash17 = hash.Get("17");
+            Console.WriteLine("18th index value: " + hash17);
+
+        }
+    }
+}
